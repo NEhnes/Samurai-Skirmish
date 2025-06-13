e77522ed1aca9a7841625fc88fe82b0cecf49d94
AyusPate412 <159576227+AyusPate412@users.noreply.github.com>
2025-06-13 15:51:06 -0400

    Added Different Screens

---
+++ Samurai Skirmish/MenuScreen.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Samurai_Skirmish
+{
+    partial class MenuScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.howToPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.playButton.BackColor = System.Drawing.Color.Transparent;
+            this.playButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.Color.Transparent;
+            this.playButton.Location = new System.Drawing.Point(383, 496);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(194, 83);
+            this.playButton.TabIndex = 4;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.exitButton.BackColor = System.Drawing.Color.Transparent;
+            this.exitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.exitButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.exitButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.exitButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.ForeColor = System.Drawing.Color.Transparent;
+            this.exitButton.Location = new System.Drawing.Point(18, 496);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(194, 83);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            // 
+            // howToPlay
+            // 
+            this.howToPlay.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.howToPlay.BackColor = System.Drawing.Color.Transparent;
+            this.howToPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.howToPlay.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.howToPlay.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.howToPlay.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.howToPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.howToPlay.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.howToPlay.ForeColor = System.Drawing.Color.Transparent;
+            this.howToPlay.Location = new System.Drawing.Point(742, 496);
+            this.howToPlay.Name = "howToPlay";
+            this.howToPlay.Size = new System.Drawing.Size(194, 83);
+            this.howToPlay.TabIndex = 8;
+            this.howToPlay.Text = "Demo";
+            this.howToPlay.UseVisualStyleBackColor = false;
+            // 
+            // MenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.howToPlay);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.playButton);
+            this.Name = "MenuScreen";
+            this.Size = new System.Drawing.Size(960, 600);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button howToPlay;
+    }
+}
